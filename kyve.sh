b55ed8ac86c258d93f56868528d4bab97a79fbc3
muhtin <90136317+muhtin@users.noreply.github.com>
2021-10-08 17:19:45 +0000

    Add files via upload

---
+++ kyve.sh
@@ -0,0 +1,19 @@
+sudo apt update && sudo apt upgrade -y
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt install npm
+npm install --global yarn
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+git clone https://github.com/KYVENetwork/kyve.git
